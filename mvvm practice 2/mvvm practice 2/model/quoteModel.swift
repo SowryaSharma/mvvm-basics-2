ff95fb30328364c90d63305b3393666feaf44918
Sowrya Mukkavilli <sowrya.m@cumulations.com>
2022-08-22 12:33:34 +0530

    model changed

--- mvvm practice 2/mvvm practice 2/model/quoteModel.swift
+++ mvvm practice 2/mvvm practice 2/model/quoteModel.swift
@@ -3,4 +3,16 @@ import Foundation
 struct quotes:Codable{
     var content:String
     var author:String
+    private enum quotecodingkeys:String,CodingKey{
+        case content = "content"
+        case author = "author"
+    }
+}
+
+extension quotes{
+    init(from decoder:Decoder) throws{
+        let info = try decoder.container(keyedBy: quotecodingkeys.self)
+        try content = info.decodeIfPresent(String.self, forKey: .content) ?? ""
+        try author = info.decodeIfPresent(String.self, forKey: .author) ?? ""
+    }
 }
